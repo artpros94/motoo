5115615c26a39f2accc5b81ac5c8375ff11922b6
artpros94 <prostakishyn.artem@icloud.com>
2020-10-18 10:54:05 +0300

    moto_project

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+
+    $('.menu__btn').on('click', function(){
+        $('.header__menu').slideToggle();
+    });
+
+    // $('.menu__btn').on('click', function(){
+    //     $('.header__menu').slideToggle();
+       
+    // });
+
+
+});
